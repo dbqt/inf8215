01b4523cad0deda40876bdb7d6c08f31b1d60716
David Tran <dt_dbqt@hotmail.com>
2017-03-17 01:24:41 -0400

    personne done

--- tp2/question1.pl
+++ tp2/question1.pl
@@ -1,176 +1,58 @@
 
+personnage(michael_jackson, homme, us, artiste, chanteur).
+personnage(mozart, homme, europe, artiste, compositeur).
+personnage(stephan_harper, homme, canada, politicien, ministre).
+personnage(john_lewis, homme, us, politicien, militant).
+personnage(cleopatre, femme, egypte, politicien, reine).
+personnage(michel_gagnon, homme, canada, professeur, web).
+personnage(michel_dagenais, homme, canada, professeur, os).
+personnage(rafal_nadal, homme, europe, athlete, tennis).
+personnage(jacques_villeneuve, homme, canada, athlete, f1).
+personnage(eugenie_bouchard, femme, canada, athlete, tennis).
+personnage(pape_francois, homme, europe, religieux, pape).
+personnage(jesus, homme, asie, religieux, jesus_christ).
+personnage(julie_snyder, femme, canada, artiste, tv).
+personnage(brad_pitt, homme, us, artiste, acteur).
+personnage(jk_rowling, femme, europe, artiste, ecrivain).
+personnage(victor_hugo, homme, europe, artiste, ecrivain).
+personnage(blanche_neige, femme, europe, fictif, conte).
+personnage(lara_croft, femme, europe, fictif, jeux).
+personnage(moise, homme, europe, religieux, prophete).
+personnage(garfield, homme, us, fictif, bande-dessinee).
+personnage(james_bond, homme, europe, fictif, film).
+personnage(mario, homme, europe, fictif, jeux).
 
-ask(homme, X) :-
-    format('est-ce un homme?', [X]),
-    read(Reponse),
-    Reponse = 'oui'.
+profession(artiste).
+profession(politicien).
+profession(professeur).
+profession(athlete).
+profession(religieux).
+profession(fictif).
 
-ask(athlete, X) :-
-format('[~w] est-ce un athlete?', [X]),
-    read(Reponse),
-    Reponse = 'oui'.
-    
-ask(artiste, X) :-
-format('est-ce un artiste?', [X]),
-    read(Reponse),
-    Reponse = 'oui'.
-    
-ask(gouverneur, X) :-
-format('est-ce un gouverneur?', [X]),
-    read(Reponse),
-    Reponse = 'oui'.
-    
-ask(politicien, X) :-
-format('est-ce un politicien?', [X]),
-    read(Reponse),
-    Reponse = 'oui'.
-    
-ask(historique, X) :-
-format('est-ce une personne historique?', [X]),
-    read(Reponse),
-    Reponse = 'oui'.
-    
-ask(personnage, X) :-
-format('est-ce un personnage fictif?', [X]),
-    read(Reponse),
-    Reponse = 'oui'.
-    
-ask(professeur, X) :-
-format('est-ce un professeur?', [X]),
-    read(Reponse),
-    Reponse = 'oui'.
-
-ask(gouverne,Y):-
-format('Gouverne ~w ? ',[Y]),
-    read(Reponse),
-    Reponse = 'oui'.
-
-
-/* entry point */
-
-personne(X) :- sexe(X,ListeHommes), categorie(ListeHommes, ). 
-    
-long([],N).
-long([_|L],[x|M]):- M is N+1,long(L,M).
-    
-sexe(X, ListeHommes) :-
-    ask(homme,X),
-    findall(X, homme(X), ListeHommes).
-
-sexe(X, Y) :-
-    femme(Y).
-    
-categorie(X) :-
-    ask(athlete,X),
-    athlete(X).
-    
-categorie(X) :-
-    ask(artiste,X),
-    artiste(X).
-    
-categorie(X) :-
-    ask(gouverneur,X),
-    gouverneur(X).
-    
-categorie(X) :-
-    ask(politicien,X),
-    politicien(X).
-    
-categorie(X) :-
-    ask(historique,X),
-    historique(X).
-    
-categorie(X) :-
-    ask(personnage,X),
-    personnage(X).
-    
-categorie(X) :-
-    ask(professeur,X),
-    professeur(X).
-
-
-/**/
-
-politicien(X) :-
-    gouverne(X,Y),
-    pays(Y),
-    ask(gouverne,Y).
-
-
-gouverne(stephen_harper,canada).
-
-gouverne(barack_obama,usa).
-
+pays(europe).
+pays(us).
 pays(canada).
+pays(asie).
+pays(egypte).
 
-pays(usa).
+sexe(homme).
+sexe(femme).
+
+ask(Sujet, X) :-
+ format("Est-ce que la personne ~w ~w ? ~n", [Sujet, X]), 
+ read(Reponse), 
+ Reponse = 'oui'.
 
 
-/* genre */
-femme(eugenie_bouchard).
-femme(jk_rowling).
-femme(cleopatre_vii).
-femme(blanche_neige).
-femme(lara_croft).
-femme(julie_snyder).
-homme(brad_pitt).
-homme(rafal_nadal).
-homme(jacque_villeneuve).
-homme(victor_hugo).
-homme(michael_jackson).
-homme(stephen_harper).
-homme(moise).
-homme(jesus).
-homme(pape_francois).
-homme(wolgang_amadeus_mozart).
-homme(gardfield).
-homme(james_bond).
-homme(mario).
-homme(john_lewis).
-homme(michel_gagnon).
-homme(michel_dagenais).
+personne(X) :- 
+    sexe(A), 
+        ask('est de sexe', A), !, 
+	pays(B), 
+        ask('vient de', B), !, 
+    profession(C), 
+        ask('est', C) , !, 
+    personnage(X, A, B, C, D), 
+        ask('est plus precisement', D), !.
 
-/ metier */
-artiste(brad_pitt).
-artiste(jk_rowling).
-artiste(victor_hugo).
-artiste(michael_jackson).
-artiste(wolgang_amadeus_mozart).
-artiste(julie_snyder).
-athlete(eugenie_bouchard).
-athlete(rafal_nada).
-athlete(jacque_villeneuve).
-gouverneur(stephen_harper).
-historique(cleopatre_vii).
-historique(moise).
-historique(jesus).
-historique(pape_francois).
-personnage(blanche_neige).
-personnage(lara_croft).
-personnage(mario).
-personnage(garfield).
-personnage(james_bond).
-politicien(john_lewis).
-professeur(michel_gagnon).
-professeur(michel_dagenais).
-
-/* specialite */
-acteur(brad_pitt).
-auteur(jk_rowling).
-auteur(victor_hugo).
-chanteur(michael_jackson).
-musicien(wolgang_amadeus_mozart).
-producteur(julie_snyder).
-tennis(eugenie_bouchard).
-soccer(rafal_nadal).
-course_char(jacque_villeneuve).
-egypte(cleopater_vii).
-egypte(moise).
-vatican(pape_francois).
-conte(blanche_neige).
-jeux(lara_croft).
-jeux(mario).
-animal(garfield).
-film(james_bond).
-web(michel_gagnon).
-os(michel_dagenais).
+personne(X) :-
+    write('Aucune personne ne correspond a ces criteres'), fail.
