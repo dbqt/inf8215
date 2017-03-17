f049e330247cb3cf243dfdec7f394fcbad96cda6
David Tran <dt_dbqt@hotmail.com>
2017-03-17 02:12:25 -0400

    done q1

--- tp2/question1.pl
+++ tp2/question1.pl
@@ -1,4 +1,55 @@
 
+/*Poser une question avec la formulation "Sujet" et la reponse "X" */
+ask(Sujet, X) :-
+ format("Est-ce que ~w ~w ? ~n", [Sujet, X]), 
+ read(Reponse), 
+ Reponse = 'oui'.
+
+/**********
+  PERSONNE
+***********/
+
+/*Verifie s'il y a une seule reponse, utile pour skipper les autres questions pour personnage */
+countAnswerP(X, A) :-
+    findall(X, personnage(X, A, _, _, _), Y),
+    length(Y, Z), 
+    Z =:= 1,
+    personnage(X, A, _, _, _).
+
+countAnswerP(X, A, B) :-
+    findall(X, personnage(X, A, B, _, _), Y),
+    length(Y, Z), 
+    Z =:= 1,
+    personnage(X, A, B, _, _).
+
+countAnswerP(X, A, B, C) :-
+    findall(X, personnage(X, A, B, C, _), Y),
+    length(Y, Z), 
+    Z =:= 1,
+    personnage(X, A, B, C, _).
+
+/*Pose des questions jusqu'a ce qu'il y a une seule reponse possible */
+personne(X) :- 
+    sexe(A), 
+    ask('la personne est de sexe', A), !, 
+    (countAnswerP(X, A);
+        pays(B), 
+        ask('la personne vient de', B), !, 
+        (countAnswerP(X, A, B);
+            profession(C), 
+            ask('la personne est', C) , !, 
+            (countAnswerP(X, A, B, C);
+                personnage(X, A, B, C, D), 
+                ask('la personne est plus precisement', D), !
+            )
+        )
+    ).
+
+/*Message pour dire qu'il y a rien qui fonctionne' */
+personne(X) :-
+    write('Aucune personne ne correspond a ces criteres'), fail.
+
+/*Donnees pour personne*/
 personnage(michael_jackson, homme, us, artiste, chanteur).
 personnage(mozart, homme, europe, artiste, compositeur).
 personnage(stephan_harper, homme, canada, politicien, ministre).
@@ -38,21 +89,80 @@ pays(egypte).
 sexe(homme).
 sexe(femme).
 
-ask(Sujet, X) :-
- format("Est-ce que la personne ~w ~w ? ~n", [Sujet, X]), 
- read(Reponse), 
- Reponse = 'oui'.
+/********
+  OBJET
+*********/
 
+/*Verifie s'il y a une seule reponse, utile pour skipper les autres questions pour objet */
+countAnswerO(X, A) :-
+    findall(X, chose(X, A, _, _, _), Y),
+    length(Y, Z), 
+    Z =:= 1,
+    chose(X, A, _, _, _).
 
-personne(X) :- 
-    sexe(A), 
-        ask('est de sexe', A), !, 
-	pays(B), 
-        ask('vient de', B), !, 
-    profession(C), 
-        ask('est', C) , !, 
-    personnage(X, A, B, C, D), 
-        ask('est plus precisement', D), !.
+countAnswerO(X, A, B) :-
+    findall(X, chose(X, A, B, _, _), Y),
+    length(Y, Z), 
+    Z =:= 1,
+    chose(X, A, B, _, _).
 
+countAnswerO(X, A, B, C) :-
+    findall(X, chose(X, A, B, C, _), Y),
+    length(Y, Z), 
+    Z =:= 1,
+    chose(X, A, B, C, _).
+
+/*Pose des questions jusqu'a ce qu'il y a une seule reponse possible */
+objet(X) :- 
+    electrique(A), 
+    ask('l\'objet est-il', A), !, 
+    (countAnswerO(X, A);
+        lieu(B), 
+        ask('l\'objet se retrouve-t-il dans', B), !, 
+        (countAnswerO(X, A, B);
+            poids(C), 
+            ask('l\'objet est-il', C) , !, 
+            (countAnswerO(X, A, B, C);
+                chose(X, A, B, C, D), 
+                ask('l\'objet est plus precisement pour', D), !
+            )
+        )
+    ).
+
+/*Message pour dire qu'il y a rien qui fonctionne' */
 personne(X) :-
     write('Aucune personne ne correspond a ces criteres'), fail.
+
+/*Donnees pour objets */
+chose(aspirateur, electronique, maison_en_general, lourd, menage).
+chose(telephone, electronique, bureau, leger, communication).
+chose(cuisiniere, electronique, cuisine, lourd, cuisson_lente).
+chose(grillepain, electronique, cuisine, leger, pain).
+chose(lampe, electronique, maison_en_general, leger, lumiere).
+chose(ordinateur, electronique, bureau, lourd, calcul).
+chose(microonde, electronique, cuisine, lourd, cuisson_rapide).
+chose(cafetiere, electronique, cuisine, leger, cafe).
+chose(balai, pas_electronique, maison_en_general, leger, menage).
+chose(assiete, pas_electronique, cuisine, leger, tenir_nourriture).
+chose(casserole, pas_electronique, cuisine, leger, cuisson).
+chose(detergent, pas_electronique, cuisine, leger, nettoyer).
+chose(cle, pas_electronique, maison_en_general, leger, ouvrir_serrure).
+chose(sac_a_dos, pas_electronique, bureau, leger, transporter).
+chose(fourchette, pas_electronique, cuisine, leger, manipuler_nourriture).
+chose(cactus, pas_electronique, maison_en_general, leger, decoration).
+chose(table, pas_electronique, cuisine, lourd, multifonctionnel).
+chose(shampoing, pas_electronique, maison_en_general, leger, nettoyer_cheveux).
+chose(lit, pas_electronique, maison_en_general, lourd, dormir).
+chose(porte_feuille, pas_electronique, bureau, leger, contenir_argent).
+chose(piano, pas_electronique, maison_en_general, lourd, musique).
+chose(papier, pas_electronique, bureau, leger, ecrire).
+
+electrique(electronique).
+electrique(pas_electronique).
+
+poids(lourd).
+poids(leger).
+
+lieu(maison_en_general).
+lieu(cuisine).
+lieu(bureau).
